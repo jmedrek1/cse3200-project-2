3ea6a3874a95835051ff45a2cc3e927b05e8e9e8
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-10-09 11:16:37 -0400

    initial commit

---
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,22 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CrimeDetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // don't call setContentView - save that for onCreateView
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding . // left off here
+    }
+}
