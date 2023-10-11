dd6b2df8a4d8b74995c5ec54f475aa7d655e2be5
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-10-11 10:58:33 -0400

    10/11 lecture code added

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
 
 class CrimeDetailFragment : Fragment() {
+    private lateinit var binding : FragmentCrimeDetailBinding
+    private lateinit var crime : Crime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // don't call setContentView - save that for onCreateView
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "Bad Stuff Crime",
+            date = Date(),
+            isSolved = false
+        )
     }
 
     override fun onCreateView(
@@ -17,6 +29,30 @@ class CrimeDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding . // left off here
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply { // all binding/wire-ups go here
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+//            crimeDate.setText(crime.title)
+//            crimeDate.setOnClickListener {
+//                crimeDate.setText(crime.title)
+//            }
+            crimeDate.apply {
+                text = crime.date.toString()
+                crimeDate.isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
     }
 }
