ce6242fa35616522f37503a5ffa18a63b82ff511
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-10-27 10:59:14 -0400

    changes from oct 27 lecture

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -7,8 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
@@ -43,7 +47,13 @@ class CrimeListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO do stuff
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,12 +1,27 @@
 package com.example.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
-
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
     init {
+        Log.d(TAG, "about to initialize our crime data")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched. expect delay")
+            crimes += loadCrimes()
+            Log.d(TAG, "Crime data should be finished")
+        }
+    }
+
+    suspend fun loadCrimes() : List<Crime> {
+//        delay(5000)
+        var result = mutableListOf<Crime>()
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -14,7 +29,8 @@ class CrimeListViewModel : ViewModel() {
                 date = Date(),
                 isSolved = i % 2 == 0
             )
-            crimes.add(crime)
+            result.add(crime)
         }
+        return result
     }
 }
