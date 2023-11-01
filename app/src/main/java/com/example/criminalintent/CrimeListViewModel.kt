cb25f99d4138c85d0fab73e9b9a84269489e3eec
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-11-01 10:59:51 -0400

    changes from 11/1 lecture

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -9,28 +9,16 @@ import java.util.Date
 import java.util.UUID
 private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
+
+    private val crimeRepository = CrimeRepository.get()
     val crimes = mutableListOf<Crime>()
     init {
-        Log.d(TAG, "about to initialize our crime data")
         viewModelScope.launch {
-            Log.d(TAG, "coroutine launched. expect delay")
-            crimes += loadCrimes()
-            Log.d(TAG, "Crime data should be finished")
+
         }
     }
 
     suspend fun loadCrimes() : List<Crime> {
-//        delay(5000)
-        var result = mutableListOf<Crime>()
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title = "Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
-            result.add(crime)
-        }
-        return result
+        return crimeRepository.getCrimes()
     }
 }
--- app/src/main/java/database/CrimeDao.kt
+++ app/src/main/java/database/CrimeDao.kt
@@ -1,9 +1,10 @@
 package database
 
+import androidx.room.Dao
 import androidx.room.Query
 import com.example.criminalintent.Crime
 import java.util.UUID
-
+@Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
     suspend fun getCrimes(): List<Crime>
